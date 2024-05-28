2441489200d242a0adac4978fed157c779603225
Yago Lacoste <yagolacos@gmail.com>
2024-05-28 12:48:47 -0300

    First commit

---
+++ #52-Hands-Exercise-09/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x = 40
+
+const y = 41
+
+func main() {
+	z := 42
+	fmt.Printf("Use value of is %v and the type of x is %T\n", x, x)
+	fmt.Printf("Use value of is %v and the type of y is %T\n", y, y)
+	fmt.Printf("Use value of is %v and the type of z is %T\n", z, z)
+
+}
